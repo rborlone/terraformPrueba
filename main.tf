e0e9358c267a02ceaa0e3ad480a6564adfecb23e
Rodrigo Borlone Espoz <rborlone@rednegocios.cl>
2022-09-15 15:57:22 -0300

    Se agrega Virtual Net

--- main.tf
+++ main.tf
@@ -6,22 +6,6 @@ resource "azurerm_resource_group" "example" {
 resource "azurerm_virtual_network" "example-vnet" {
   name                = "example-vnet"
   resource_group_name = azurerm_resource_group.example.name
-  address_space       = ["10.0.1.0/24"]
+  address_space       = var.address_space
   location            = azurerm_resource_group.example.location
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "example-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
-
-  delegation {
-    name = "delegation"
-
-    service_delegation {
-      name    = "Microsoft.ContainerInstance/containerGroups"
-      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
-    }
-  }
 }
--- variables.tf
+++ variables.tf
@@ -5,3 +5,7 @@ variable "location" {
 variable "rg_name" {
   default = "rg-tf-test"
 }
+
+variable "address_space"{
+  default = ["172.20.0.0/16"]
+}
