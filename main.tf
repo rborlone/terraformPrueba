b067a543993847ee25da6774d420353bd2301641
Rodrigo Borlone <rborlone@rednegocios.cl>
2022-09-16 10:10:07 -0300

    Demostracion de una VNET

--- main.tf
+++ main.tf
@@ -9,3 +9,72 @@ resource "azurerm_virtual_network" "example-vnet" {
   address_space       = var.address_space
   location            = azurerm_resource_group.example.location
 }
+
+resource "azurerm_subnet" "example-subnet" {
+  name                 = "example-subnet"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example-vnet.name
+  address_prefixes     = var.address_subnet_space_aks
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
+}
+
+resource "azurerm_subnet" "example-subnet-2" {
+  name                 = "example-subnet-2"
+  resource_group_name  = azurerm_resource_group.example.name
+  virtual_network_name = azurerm_virtual_network.example-vnet.name
+  address_prefixes     = var.address_subnet-space-ingress
+
+  delegation {
+    name = "delegation"
+
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
+    }
+  }
+}
+
+resource "azurerm_virtual_network_peering" "example-peer-1" {
+  name                      = "peer1to2"
+  resource_group_name       = azurerm_resource_group.example.name
+  virtual_network_name      = azurerm_virtual_network.example-vnet.name
+  remote_virtual_network_id = var.id-vnet-firewall
+}
+
+resource "azurerm_virtual_network_peering" "example-peer-2" {
+  name                      = "peer2to1"
+  resource_group_name       = var.name-rg-firewall
+  virtual_network_name      = var.name-vnet-firewall
+  remote_virtual_network_id = azurerm_virtual_network.example-vnet.id
+}
+
+resource "azurerm_route_table" "example" {
+  name                          = "example-route-table"
+  location                      = azurerm_resource_group.example.location
+  resource_group_name           = azurerm_resource_group.example.name
+  disable_bgp_route_propagation = false
+
+  route {
+    name           = "allinternetfw"
+    address_prefix = "0.0.0.0/0"
+    next_hop_type  = "VirtualAppliance"
+    next_hop_in_ip_address = var.ip-internal-firewall
+  }
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_subnet_route_table_association" "example" {
+  subnet_id      = azurerm_subnet.example-subnet.id
+  route_table_id = azurerm_route_table.example.id
+}
--- variables.tf
+++ variables.tf
@@ -7,5 +7,29 @@ variable "rg_name" {
 }
 
 variable "address_space"{
-  default = ["172.20.0.0/16"]
+  default = ["172.21.0.0/16"]
+}
+
+variable "address_subnet_space_aks"{
+  default = ["172.21.1.0/24"]
+}
+
+variable "address_subnet-space-ingress"{
+  default = ["172.21.2.0/24"]
+}
+
+variable "name-rg-firewall"{
+  default = "rg-AccesoRedNegocios"
+}
+
+variable "ip-internal-firewall"{
+  default = "172.18.0.4"
+}
+
+variable "id-vnet-firewall"{
+  default = "/subscriptions/eec1fafa-da18-4aa2-8abf-858f269b9331/resourceGroups/rg-AccesoRedNegocios/providers/Microsoft.Network/virtualNetworks/vnet-firewall"
+}
+
+variable "name-vnet-firewall"{
+  default="vnet-firewall"
 }
